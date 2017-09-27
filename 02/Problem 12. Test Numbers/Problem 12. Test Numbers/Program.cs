64aa3a959a889ca7a34df2cd535d85f13829f68c
vld62 <iliev62@gmail.com>
2017-09-27 12:37:22 -0700

    addding last tasks from Chapter 2

---
+++ 02/Problem 12. Test Numbers/Problem 12. Test Numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Problem_12.Test_Numbers
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var n = int.Parse(Console.ReadLine());
+            var m = int.Parse(Console.ReadLine());
+            var maxSum = int.Parse(Console.ReadLine());
+            int combinations = 0;
+            int sum = 0;
+            for (int i = n; i >= 1; i--)
+            {
+
+                for (int j = 1; j <= m; j++)
+                {
+                    if (sum <= maxSum)
+                    {
+                        sum = sum + (i * j * 3);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    combinations++;
+                }
+            }
+            if (sum >= maxSum)
+            {
+                Console.WriteLine($"{combinations} combinations");
+                Console.WriteLine($"Sum: {sum} >= {maxSum}");
+            }
+            else
+            {
+                Console.WriteLine($"{combinations} combinations");
+                Console.WriteLine($"Sum: {sum}");
+            }
+        }
+    }
+}
