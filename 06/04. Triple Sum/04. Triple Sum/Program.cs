29b73c89d96f1f4f52d0553cc493d323e62bd947
vld62 <iliev62@gmail.com>
2017-10-07 02:57:14 -0700

    adding tasks from 06

--- 06/04. Triple Sum/04. Triple Sum/Program.cs
+++ 06/04. Triple Sum/04. Triple Sum/Program.cs
@@ -1,15 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _04.Triple_Sum
+﻿namespace _04.Triple_Sum
 {
+    using System;
+    using System.Linq;
     class Program
     {
         static void Main(string[] args)
         {
+
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    int sum = arr[i] + arr[j];
+                    if (arr.Contains(sum))
+                    {
+                        Console.WriteLine("{0} + {1} == {2}", arr[i], arr[j], sum);
+                        count++;
+                        
+                    }
+
+                }
+            }
+
+            if (count == 0) {
+                Console.WriteLine("No");
+            }
         }
     }
 }
---
+++ 06/05. Rounding Numbers/05. Rounding Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _05.Rounding_Numbers
+{
+    using System;
+    using System.Linq;
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double[] arr = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            double[] arr2 = arr;
+      
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                Console.WriteLine("{0} => {1}", arr[i],
+                arr2[i] = Math.Round(arr2[i], 0, MidpointRounding.AwayFromZero));
+            }
+        }
+    }
+}
