1526949dfb0cfea9cc6d386ebecf233ff000211d
vld62 <iliev62@gmail.com>
2017-10-10 14:04:17 -0700

    Adding tasks from 06

---
+++ 06/8. Most Frequent Number/8. Most Frequent Number/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.Most_Frequent_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int mostFrequentNumber = 0;
+            int repetitions = 0;
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+
+                int currentNumber = arr1[i];
+                int counter = 0;
+                for (int j = i; j < arr1.Length; j++)
+                {
+                    if (currentNumber == arr1[j])
+                    {
+
+                        counter++;
+                    }
+
+                }
+
+                if (counter > repetitions)
+                {
+
+
+                    mostFrequentNumber = currentNumber;
+                    repetitions = counter;
+
+
+                }
+
+            }
+            Console.WriteLine(mostFrequentNumber);
+        }
+    }
+}
---
+++ 06/9. Index of Letters/9. Index of Letters/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _09.Index_of_Letters
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] lettersArray = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            char[] wordArray = Console.ReadLine().ToLower().ToCharArray();
+
+            for (int i = 0; i < wordArray.Length; i++)
+            {
+                for (int j = 0; j < lettersArray.Length; j++)
+                {
+                    if (wordArray[i] == lettersArray[j])
+                    {
+                        Console.WriteLine("{0} -> {1}", wordArray[i], j);
+                    }
+                }
+            }
+        }
+    }
+}
