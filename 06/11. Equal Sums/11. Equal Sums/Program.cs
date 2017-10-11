4514d319ca0a27299fe927251ffc8000a02f3d2e
vld62 <iliev62@gmail.com>
2017-10-11 11:06:22 -0700

    Adding tasks from 06

---
+++ 06/11. Equal Sums/11. Equal Sums/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace _11.Equal_Sums
+{
+    using System;
+    using System.Linq;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array1 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int leftSum = 0;
+            int rightSum = 0;
+            bool equalSumNotFound = true;
+
+            for (int i = 0; i <= Array1.Length - 1; i++)
+            {
+                if (Array1.Length == 1)
+                {
+                    Console.WriteLine(0);
+                    equalSumNotFound = false;
+                    break;
+                }
+                else
+                {
+                    leftSum = 0;
+                    for (int j = i; 0 < j; j--)
+                    {
+                        if (j > 0)
+                        {
+                            leftSum += Array1[j - 1];
+                        }
+                    }
+                    rightSum = 0;
+                    for (int j = i; j < Array1.Length; j++)
+                    {
+                        if (j < Array1.Length - 1)
+                        {
+                            rightSum += Array1[j + 1];
+                        }
+                    }
+
+                    if (leftSum == rightSum)
+                    {
+                        Console.WriteLine(i);
+                        equalSumNotFound = false;
+                        break;
+                    }
+                }
+            }
+            if (equalSumNotFound)
+            {
+                Console.WriteLine("no");
+            }
+
+        }
+    }
+}
---
+++ 06/5. Compare Char Arrays/5. Compare Char Arrays/Program.cs
@@ -0,0 +1,146 @@
+﻿namespace _5.Compare_Char_Arrays
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] wordArray1 = Console.ReadLine().ToLower().Replace(" ", "").ToCharArray();
+            char[] wordArray2 = Console.ReadLine().ToLower().Replace(" ", "").ToCharArray();
+
+
+            if (wordArray1[0] == wordArray2[0])
+            {
+                if (wordArray1.Length > wordArray2.Length)
+                {
+                    for (int i = 0; i < wordArray2.Length; i++)
+                    {
+                        if (wordArray1[i] < wordArray2[i])
+                        {
+                            for (int j = 0; j < wordArray2.Length; j++)
+                            {
+
+                                Console.Write(wordArray1[j]);
+
+                            }
+                            Console.WriteLine();
+                            for (int j = 0; j < wordArray2.Length; j++)
+                            {
+
+                                Console.Write(wordArray2[j]);
+
+                            }
+
+                            break;
+                        }
+                        else
+                        {
+                            for (int j = 0; j < wordArray2.Length; j++)
+                            {
+
+                                Console.Write(wordArray2[j]);
+
+                            }
+                            Console.WriteLine();
+                            for (int j = 0; j < wordArray1.Length; j++)
+                            {
+
+                                Console.Write(wordArray1[j]);
+
+                            }
+
+                            break;
+
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < wordArray1.Length; i++)
+                    {
+                        if (wordArray1[i] < wordArray2[i])
+                        {
+                            for (int j = 0; j < wordArray2.Length; j++)
+                            {
+
+                                Console.Write(wordArray2[j]);
+
+                            }
+                            Console.WriteLine();
+                            for (int j = 0; j < wordArray1.Length; j++)
+                            {
+
+                                Console.Write(wordArray1[j]);
+
+                            }
+
+                            break;
+                        }
+                        else
+                        {
+                            for (int j = 0; j < wordArray1.Length; j++)
+                            {
+
+                                Console.Write(wordArray1[j]);
+
+                            }
+                            Console.WriteLine();
+                            for (int j = 0; j < wordArray2.Length; j++)
+                            {
+
+                                Console.Write(wordArray2[j]);
+
+                            }
+
+                            break;
+
+                        }
+                    }
+
+                }
+
+
+            }
+            else
+            {
+                if (wordArray1[0] < wordArray2[0])
+                {
+                    for (int i = 0; i < wordArray1.Length; i++)
+                    {
+
+                        Console.Write(wordArray1[i]);
+
+                    }
+                    Console.WriteLine();
+                    for (int i = 0; i < wordArray1.Length; i++)
+                    {
+
+                        Console.Write(wordArray2[i]);
+
+                    }
+
+                }
+                else
+                {
+                    for (int i = 0; i < wordArray2.Length; i++)
+                    {
+                        Console.Write(wordArray2[i]);
+
+                    }
+                    Console.WriteLine();
+                    for (int i = 0; i < wordArray1.Length; i++)
+                    {
+                        Console.Write(wordArray1[i]);
+
+                    }
+
+
+                }
+
+
+            }
+        }
+    }
+}
