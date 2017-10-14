7a63653f92b2f0954aad1c2891c1e7b449ba1171
vld62 <iliev62@gmail.com>
2017-10-14 09:30:31 -0700

    Adding tasks from 08

--- 08/05. Short Words Sorted/05. Short Words Sorted/Program.cs
+++ 08/05. Short Words Sorted/05. Short Words Sorted/Program.cs
@@ -1,15 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _05.Short_Words_Sorted
+﻿namespace _05.Short_Words_Sorted
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
     class Program
     {
+  
         static void Main(string[] args)
         {
+            var arrayOfWords = Console.ReadLine()
+                .ToLower()
+                .Split(new char[] { '.', ',', ':', ';', '(', ')', '[', ']', '"', '\'', '\\', '/', '!', '?', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            var smallWords = new List<string>();
+
+            foreach (var word in arrayOfWords)
+            {
+                if (word.Length < 5)
+                {
+                    smallWords.Add(word);
+                }
+            }
+            var result = smallWords
+                .OrderBy(w => w)
+                .Distinct();
+
+            Console.WriteLine(string.Join(", ", result));
+
         }
     }
 }
+
---
+++ 08/06. Fold and Sum/06. Fold and Sum/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _06.Fold_and_Sum
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int k = arr.Length / 4;
+            var rowLeft = arr.Take(k).Reverse();
+            var rowRight = arr.Reverse().Take(k);
+            var row1 = rowLeft.Concat(rowRight).ToArray();
+            var row2 = arr.Skip(k).Take(2 * k).ToArray();
+            var sumArr = row1.Select((x, index) => x + row2[index]);
+            Console.WriteLine(string.Join(" ", sumArr));
+        }
+    }
+}
