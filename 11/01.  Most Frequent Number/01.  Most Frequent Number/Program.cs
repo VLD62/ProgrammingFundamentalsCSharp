c2671feeea798536d947517a1cdcefadbf0b51b4
vld62 <iliev62@gmail.com>
2017-10-22 19:43:08 +0300

    Adding tasks from 11

---
+++ 11/01.  Most Frequent Number/01.  Most Frequent Number/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _01.Most_Frequent_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string contents = File.ReadAllText("C:\\Users\\Win10\\Dev\\Input.txt");
+
+            int[] numbers = contents.Split(' ').Select(int.Parse).ToArray();
+            int counter = 0;
+            int number = 0;
+            int maxCounter = 0;
+            bool max = false;
+
+
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] == numbers[j] && !max)
+                    {
+                        counter++;
+
+                        if (counter > maxCounter)
+                        {
+                            number = numbers[i];
+                            maxCounter = counter;
+                            max = true;
+                        }
+                        else
+                        {
+                            counter = 0;
+                        }
+
+                    }
+                }
+            }
+            using (StreamWriter outputFile = new StreamWriter("C:\\Users\\Win10\\Dev\\Output.txt"))
+            {
+                outputFile.WriteLine(number);
+            }
+        }
+    }
+}
