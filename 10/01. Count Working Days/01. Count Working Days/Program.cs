d399c6dc36672a4654e8b031f378a662da3a8c72
vld62 <iliev62@gmail.com>
2017-10-22 15:56:11 +0300

    Adding tasks from 10

---
+++ 10/01. Count Working Days/01. Count Working Days/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _01.Count_Working_Days
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime startDate = DateTime.ParseExact(Console.ReadLine(), "dd-MM-yyyy", CultureInfo.InvariantCulture);
+            DateTime endDate = DateTime.ParseExact(Console.ReadLine(), "dd-MM-yyyy", CultureInfo.InvariantCulture);
+
+            var excludeDates = new List<DateTime>();
+            excludeDates.Add(new DateTime(endDate.Year, 1, 1));
+            excludeDates.Add(new DateTime(endDate.Year, 3, 3));
+            excludeDates.Add(new DateTime(endDate.Year, 5, 1));
+            excludeDates.Add(new DateTime(endDate.Year, 5, 6));
+            excludeDates.Add(new DateTime(endDate.Year, 5, 24));
+            excludeDates.Add(new DateTime(endDate.Year, 9, 6));
+            excludeDates.Add(new DateTime(endDate.Year, 9, 22));
+            excludeDates.Add(new DateTime(endDate.Year, 11, 1));
+            excludeDates.Add(new DateTime(endDate.Year, 12, 24));
+            excludeDates.Add(new DateTime(endDate.Year, 12, 25));
+            excludeDates.Add(new DateTime(endDate.Year, 12, 26));
+
+
+            int count = 0;
+            for (DateTime index = startDate; index <= endDate; index = index.AddDays(1))
+            {
+                if (index.DayOfWeek != DayOfWeek.Sunday && index.DayOfWeek != DayOfWeek.Saturday)
+                {
+                    bool excluded = false; ;
+                    for (int i = 0; i < excludeDates.Count; i++)
+                    {
+                        if (index.Month.CompareTo(excludeDates[i].Month) == 0 && index.Day.CompareTo(excludeDates[i].Day) == 0)
+                        {
+                            excluded = true;
+                            break;
+                        }
+                    }
+                    if (!excluded)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ 10/04. Average Grades/04. Average Grades/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _04.Average_Grades
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    class Student {
+
+        public string Name;
+        public List<double> Grades = new List<double>();
+        public double AverageGrade;
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            List<Student>Students = new List<Student>();
+            for (int i = 0; i < num; i++)
+            {
+                string[] studentData = Console.ReadLine().Split().ToArray();
+                Student student = new Student();
+                student.Name = studentData[0];
+
+                for (int j = 1; j < studentData.Length; j++)
+                {
+                    student.Grades.Add(Convert.ToDouble(studentData[j]));
+                }
+
+                student.AverageGrade = student.Grades.Average();
+
+                if (student.AverageGrade >= 5.00)
+                {
+                    Students.Add(student);
+                }
+            }
+            Students = Students.OrderBy(x => x.Name).ThenByDescending(x => x.AverageGrade).ToList();
+
+            foreach (var zubar in Students)
+            {
+                Console.WriteLine("{0} -> {1:f2}", zubar.Name, zubar.AverageGrade);
+            }
+        }
+    }
+}
