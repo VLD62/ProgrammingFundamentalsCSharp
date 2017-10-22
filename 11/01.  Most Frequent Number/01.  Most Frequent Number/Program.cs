1f0300548d12ab21898688336f1178d204393fee
vld62 <iliev62@gmail.com>
2017-10-22 19:47:56 +0300

    Adding tasks from 11

--- 11/01.  Most Frequent Number/01.  Most Frequent Number/Program.cs
+++ 11/01.  Most Frequent Number/01.  Most Frequent Number/Program.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
+﻿
 
 namespace _01.Most_Frequent_Number
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.IO;
+
     class Program
     {
         static void Main(string[] args)
---
+++ 11/02. Index of Letters/02. Index of Letters/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _02.Index_of_Letters
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.IO;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string contents = File.ReadAllText("C:\\Users\\Win10\\Dev\\Input.txt");
+
+            char[] lettersArray = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            char[] wordArray = contents.ToLower().ToCharArray();
+
+
+            using (StreamWriter outputFile = new StreamWriter("C:\\Users\\Win10\\Dev\\Output.txt"))
+            {
+
+                for (int i = 0; i < wordArray.Length; i++)
+                {
+                    for (int j = 0; j < lettersArray.Length; j++)
+                    {
+                        if (wordArray[i] == lettersArray[j])
+                        {
+                            outputFile.WriteLine("{0} -> {1}", wordArray[i], j);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
